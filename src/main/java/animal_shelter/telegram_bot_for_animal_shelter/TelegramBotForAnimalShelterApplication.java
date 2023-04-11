a02a7f61bb92a4c498aa161d0c05acc314baa0d1
zikit <zikit.start@yandex.ru>
2023-04-11 15:42:36 +0300

    Первый коммит.

---
+++ src/main/java/animal_shelter/telegram_bot_for_animal_shelter/TelegramBotForAnimalShelterApplication.java
@@ -0,0 +1,13 @@
+package animal_shelter.telegram_bot_for_animal_shelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotForAnimalShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotForAnimalShelterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/animal_shelter/telegram_bot_for_animal_shelter/TelegramBotForAnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package animal_shelter.telegram_bot_for_animal_shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotForAnimalShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
