7a9a797b860710de2a914a16993f27ae85db8171
Proofee <proofee@mail.ru>
2023-04-24 19:04:24 +0300

    Аннотации и несколько методов

--- src/main/java/animal_shelter/telegram_bot_for_animal_shelter/repository/UserRepository.java
+++ src/main/java/animal_shelter/telegram_bot_for_animal_shelter/repository/UserRepository.java
@@ -4,7 +4,11 @@ import animal_shelter.telegram_bot_for_animal_shelter.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
+    //Получение пользователя по chatId
+    Optional<User> findUserByChatId(Long chatId);
 }
