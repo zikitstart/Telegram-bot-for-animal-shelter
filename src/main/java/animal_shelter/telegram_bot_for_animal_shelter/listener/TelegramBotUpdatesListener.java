87e9742b6888ec79c250d0b7510b20b175ab1786
zikit <zikit.start@yandex.ru>
2023-04-26 20:47:43 +0300

    Изменена обработка сообщений.

--- src/main/java/animal_shelter/telegram_bot_for_animal_shelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/animal_shelter/telegram_bot_for_animal_shelter/listener/TelegramBotUpdatesListener.java
@@ -1,10 +1,12 @@
 package animal_shelter.telegram_bot_for_animal_shelter.listener;
 
-import animal_shelter.telegram_bot_for_animal_shelter.service.keyboard.KeyboardMenu;
+import animal_shelter.telegram_bot_for_animal_shelter.service.keyboard.KeyboardDog;
+import animal_shelter.telegram_bot_for_animal_shelter.service.keyboard.KeyboardCat;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.model.request.*;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -15,10 +17,14 @@ import java.util.List;
 @Slf4j
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private final KeyboardMenu keyboardMenu;
+    private final TelegramBot telegramBot;
+    private final KeyboardCat keyboardCat;
+    private final KeyboardDog keyboardDog;
 
-    public TelegramBotUpdatesListener(TelegramBot telegramBot, KeyboardMenu keyboardMenu) {
-        this.keyboardMenu = keyboardMenu;
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, KeyboardCat keyboardCat, KeyboardDog keyboardDog) {
+        this.telegramBot = telegramBot;
+        this.keyboardCat = keyboardCat;
+        this.keyboardDog = keyboardDog;
         telegramBot.setUpdatesListener(this);
     }
 
@@ -32,32 +38,97 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     private void handleUpdate(Update update){
         //Без обёртки периодически выбрасывает NPE.
         try {
-            if (update.message().text().equals("/start")){
-                keyboardMenu.handleHello(update);
+            if (update.message() != null && update.message().text().startsWith("/start")){
+                InlineKeyboardButton cat = new InlineKeyboardButton("Приют для кошек").callbackData("/cat");
+                InlineKeyboardButton dog = new InlineKeyboardButton("Приют для собак").callbackData("/dog");
+                InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup(cat,dog);
+                this.telegramBot.execute(new SendMessage(update.message().chat().id(),
+                        "Приветствую пользователь, это телеграмм-бот для приюта домашних животных.\n\n Пожалуйста выбери приют:").replyMarkup(keyboard));
+                return;
             }
-            if (update.message().text().equals("/cat")){
-                keyboardMenu.handleCatShelter(update);
+            if (update.callbackQuery().data().startsWith("/cat")){
+                keyboardCat.handleCatShelter(update);
+                return;
             }
-            if (update.message().text().equals("/dog")){
-                keyboardMenu.handleDogShelter(update);
+            if (update.callbackQuery().data().startsWith("/infoCat")){
+                keyboardCat.userInfoCatButtons(update);
+                return;
             }
-            if (update.message().text().equals("/info")){
-                keyboardMenu.handleInfoShelter(update);
+            if (update.callbackQuery().data().startsWith("/takeCat")){
+                keyboardCat.userTakeCatButtons(update);
+                return;
             }
-            if (update.message().text().equals("/take_cat")){
-                keyboardMenu.handleTakeCatShelter(update);
+            if (update.callbackQuery().data().startsWith("/visitingCat")){
+                keyboardCat.handleVisitingCatShelter(update);
+                return;
             }
-            if (update.message().text().equals("/take_dog")){
-                keyboardMenu.handleTakeDogShelter(update);
+            if (update.callbackQuery().data().startsWith("/registrationPassCat")){
+                keyboardCat.handleRegistrationPassCatShelter(update);
+                return;
             }
-            if (update.message().text().equals("/report")){
-                keyboardMenu.handleReportShelter(update);
+            if (update.callbackQuery().data().startsWith("/safetyPrecautionsCat")){
+                keyboardCat.handleSafetyPrecautionsCatShelter(update);
+                return;
             }
-            if (update.message().text().equals("/volunteer")){
-                keyboardMenu.handleVolunteerShelter(update);
+            if (update.callbackQuery().data().startsWith("/datingRulesCat")){
+                keyboardCat.handleDatingRulesCatShelter(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/listDocumentsCat")){
+                keyboardCat.handleListDocumentsCatShelter(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/transportingCat")){
+                keyboardCat.handleTransportingCatShelter(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/recommendationsKitten")){
+                keyboardCat.handleRecommendationsKittenShelter(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/adultCat")){
+                keyboardCat.handleAdultCatShelter(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/withDisabilitiesCat")){
+                keyboardCat.handleWithDisabilitiesCatShelter(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/reasonsRefusalCat")){
+                keyboardCat.handleReasonsRefusalCatShelter(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/reportCat")){
+                keyboardCat.handleReportCatShelter(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/volunteerCat")){
+                keyboardCat.handleVolunteerCatShelter(update);
+            }
+
+
+
+            if (update.callbackQuery().data().startsWith("/dog")){
+                keyboardDog.handleDogShelter(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/infoDog")){
+                keyboardDog.userInfoDogButtons(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/take_dog")){
+                keyboardDog.userTakeDogButtons(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/reportDog")){
+                keyboardDog.handleReportShelter(update);
+                return;
+            }
+            if (update.callbackQuery().data().startsWith("/volunteerDog")){
+                keyboardDog.handleVolunteerShelter(update);
             }
         }catch (NullPointerException e){
-            e.getMessage();
+            log.error("Была ошибка NullPointerException" + e.getMessage());
         }
     }
 }
