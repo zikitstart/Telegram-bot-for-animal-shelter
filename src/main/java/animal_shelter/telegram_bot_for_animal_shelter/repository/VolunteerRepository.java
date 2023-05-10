a93800cfad93fc59ea7e1320a69e0a69be6e95eb
zikit <zikit.start@yandex.ru>
2023-05-10 14:22:19 +0300

    Добавлены выборки волонтёров в обработчик и метод в репозиторий.

--- src/main/java/animal_shelter/telegram_bot_for_animal_shelter/repository/VolunteerRepository.java
+++ src/main/java/animal_shelter/telegram_bot_for_animal_shelter/repository/VolunteerRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
 
+    Volunteer findVolunteerByChatId(Long chatId);
 }
