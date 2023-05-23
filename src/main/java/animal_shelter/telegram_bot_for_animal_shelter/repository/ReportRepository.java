59920f97d51a0dae1c28249f32e2483b2bbab77b
zikit <zikit.start@yandex.ru>
2023-05-23 20:46:23 +0300

    Исправления контроллера.

--- src/main/java/animal_shelter/telegram_bot_for_animal_shelter/repository/ReportRepository.java
+++ src/main/java/animal_shelter/telegram_bot_for_animal_shelter/repository/ReportRepository.java
@@ -1,16 +1,18 @@
 package animal_shelter.telegram_bot_for_animal_shelter.repository;
 
+import animal_shelter.telegram_bot_for_animal_shelter.model.Pet;
 import animal_shelter.telegram_bot_for_animal_shelter.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
     // Поиск отчёта по petId
-    Report findReportByPetId(Long petId);
+    List<Report> findReportByPetId(Pet petId);
 
     // Поиск отчёта по reportId
     Report findReportByReportId(Long reportId);
